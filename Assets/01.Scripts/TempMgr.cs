71af74e77352452af319d08cd7dbdaf828df92a0
JaeWoo <fjds600@Naver.com>
2019-05-14 00:31:32 +0900

    재우 임시 작업

---
+++ Assets/01.Scripts/TempMgr.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempMgr : MonoBehaviour
+{
+
+	public GameObject[] floor;
+	public GameObject[] Cube;
+	public GameObject[] Cylinder;
+
+	void Start()
+    {
+		Cylinder[0].transform.position = Vector3.zero;
+
+		for(int i = 0; i < Cube.Length; ++i)
+		{
+
+			if(0 == i%2 )
+			{
+				asd(Cube[i]);
+			}
+		}
+
+	}
+
+	void asd(GameObject _gameObject)
+	{
+		_gameObject.transform.position = new Vector3(1, _gameObject.transform.position.y, _gameObject.transform.position.z);
+	}
+}
